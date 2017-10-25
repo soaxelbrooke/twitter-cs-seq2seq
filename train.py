35957d6de1df13229a8f3b037795e57d54086370
soaxelbrooke <stuart@axelbrooke.com>
2017-10-25 11:21:07 +0900

    Make batch size and learning rate env vars

--- train.py
+++ train.py
@@ -15,14 +15,14 @@ import toolz
 
 S2S_PARAMS = Seq2SeqConfig(
     message_len=30,
-    batch_size=32,
+    batch_size=int(os.environ.get('BATCH_SIZE', '32')),
     context_size=200,
     embed_size=200,
     use_cuda=True,
     vocab_size=2**13,
     start_token='__start__',
     encoder_layers=int(os.environ.get('ENCODER_LAYERS', '2')),
-    learning_rate=0.005
+    learning_rate=float(os.environ.get('LEARNING_RATE', '0.5')),
 )
 
 
